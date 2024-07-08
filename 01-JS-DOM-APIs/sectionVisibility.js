e18ff07b8ebaee96474ec50eced23acdaef7348e
bruno-gorosito <125034173+bruno-gorosito@users.noreply.github.com>
2024-07-08 17:48:50 -0300

    JS - Actividad 3

--- 01-JS-DOM-APIs/sectionVisibility.js
+++ 01-JS-DOM-APIs/sectionVisibility.js
@@ -1,10 +1,58 @@
+
+section = document.getElementById('hidden-section')
+
 document.addEventListener('DOMContentLoaded', () => {
-    section = document.getElementById('hidden-section')
     section.classList.toggle('visible')
 })
 
 
+function ajaxCalls(config) {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest()
+        xhr.open(config.method, config.url)
+        xhr.send()
 
-const alertar = () => {
-    alert('Soy un mensaje de alerta')
+
+        xhr.onload = function () {
+            if (xhr.status == 200) {
+                resolve({
+                    status: xhr.status,
+                    statusText: xhr.statusText,
+                    data: JSON.parse(xhr.response)
+                })
+            } else {
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                })
+            }
+        }
+
+        xhr.onerror = function() {
+            reject({
+                status: xhr.status,
+                statusText: xhr.statusText
+            })
+        }
+
+    })
+}
+
+
+const llamarAChuck = async() => {
+    try {
+        const response = await ajaxCalls({
+            method: 'get',
+            url: 'https://api.chucknorris2.io/jokes/random'
+        })
+    
+        if (response.status == 200) {
+            section.innerText = `Chuck Norris dice: "${response.data.value}"`
+        }
+    } catch (error) {
+        section.innerText = `Error al alertar a Chuck Norris.`
+        section.style.color = 'red'
+    }
+
+
 }
