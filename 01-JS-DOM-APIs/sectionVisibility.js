11592ba195b0e004d9c840196d96a5a9b52d0c25
bruno-gorosito <125034173+bruno-gorosito@users.noreply.github.com>
2024-07-08 17:34:36 -0300

    JS - Actividad 2

--- 01-JS-DOM-APIs/sectionVisibility.js
+++ 01-JS-DOM-APIs/sectionVisibility.js
@@ -2,3 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     section = document.getElementById('hidden-section')
     section.classList.toggle('visible')
 })
+
+
+
+const alertar = () => {
+    alert('Soy un mensaje de alerta')
+}
